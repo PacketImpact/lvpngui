4c74d01e79170ee289ba44a066e0febb1d44c67e
Alice <alice@packetimpact.net>
2016-08-12 03:43:27 +0200

    Update fr translation

--- translations/lvpngui_fr.ts
+++ translations/lvpngui_fr.ts
@@ -4,23 +4,28 @@
 <context>
     <name>AuthDialog</name>
     <message>
-        <location filename="../authdialog.ui" line="24"/>
+        <location filename="../src/authdialog.ui" line="31"/>
         <source>Username:</source>
         <translation>Identifiant:</translation>
     </message>
     <message>
-        <location filename="../authdialog.ui" line="34"/>
+        <location filename="../src/authdialog.ui" line="41"/>
         <source>Password:</source>
         <translation>Mot de passe:</translation>
     </message>
     <message>
-        <location filename="../authdialog.ui" line="58"/>
+        <location filename="../src/authdialog.ui" line="83"/>
+        <source>Remember</source>
+        <translation>S&apos;en souvenir</translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.ui" line="24"/>
         <source>VPN Auth</source>
         <translation>Authentification au VPN</translation>
     </message>
     <message>
-        <location filename="../authdialog.ui" line="14"/>
-        <location filename="../authdialog.cpp" line="10"/>
+        <location filename="../src/authdialog.ui" line="14"/>
+        <location filename="../src/authdialog.cpp" line="10"/>
         <source>Authentication</source>
         <translation>Authentification</translation>
     </message>
@@ -32,18 +37,18 @@
         <translation type="vanished">Log</translation>
     </message>
     <message>
-        <location filename="../logwindow.ui" line="24"/>
+        <location filename="../src/logwindow.ui" line="44"/>
         <source>Close</source>
         <translation>Fermer</translation>
     </message>
     <message>
-        <location filename="../logwindow.ui" line="31"/>
+        <location filename="../src/logwindow.ui" line="37"/>
         <source>Copy</source>
         <translation>Copier</translation>
     </message>
     <message>
-        <location filename="../logwindow.ui" line="14"/>
-        <location filename="../logwindow.cpp" line="15"/>
+        <location filename="../src/logwindow.ui" line="14"/>
+        <location filename="../src/logwindow.cpp" line="15"/>
         <source>Log</source>
         <translation>Log</translation>
     </message>
@@ -51,35 +56,61 @@
 <context>
     <name>OpenVPN</name>
     <message>
-        <location filename="../openvpn.cpp" line="67"/>
         <source>Connecting...</source>
-        <translation>Connexion...</translation>
+        <translation type="vanished">Connexion...</translation>
     </message>
     <message>
-        <location filename="../openvpn.cpp" line="99"/>
         <source>Disconnecting...</source>
-        <translation>Déconnexion...</translation>
+        <translation type="vanished">Déconnexion...</translation>
     </message>
     <message>
-        <location filename="../openvpn.cpp" line="138"/>
+        <location filename="../src/openvpn.cpp" line="120"/>
+        <source>Status:</source>
+        <translation>État:</translation>
+    </message>
+    <message>
+        <location filename="../src/openvpn.cpp" line="143"/>
         <source>Error:</source>
         <translation>Erreur:</translation>
     </message>
     <message>
-        <location filename="../openvpn.cpp" line="142"/>
+        <location filename="../src/openvpn.cpp" line="147"/>
         <source>Finished:</source>
         <translation>Terminé:</translation>
     </message>
 </context>
 <context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../src/openvpn.cpp" line="253"/>
+        <source>Connected.</source>
+        <translation>Connecté.</translation>
+    </message>
+    <message>
+        <location filename="../src/openvpn.cpp" line="255"/>
+        <source>Connecting...</source>
+        <translation>Connexion...</translation>
+    </message>
+    <message>
+        <location filename="../src/openvpn.cpp" line="257"/>
+        <source>Disconnected.</source>
+        <translation>Déconnecté.</translation>
+    </message>
+    <message>
+        <location filename="../src/openvpn.cpp" line="259"/>
+        <source>Disconnecting...</source>
+        <translation>Déconnexion...</translation>
+    </message>
+</context>
+<context>
     <name>QCoreApplication</name>
     <message>
-        <location filename="../installer.cpp" line="234"/>
+        <location filename="../src/installer.cpp" line="246"/>
         <source>%1 is already running. Please close it to upgrade.</source>
         <translation>%1 est déjà en cours de fonctionnement. Quittez pour continuer la mise à jour.</translation>
     </message>
     <message>
-        <location filename="../installer.cpp" line="260"/>
+        <location filename="../src/installer.cpp" line="272"/>
         <source>%1 has been installed. Create a desktop shortcut?</source>
         <translation>%1 a été installé. Créer un raccourci sur le bureau ?</translation>
     </message>
@@ -87,182 +118,253 @@
 <context>
     <name>SettingsWindow</name>
     <message>
-        <location filename="../settingswindow.ui" line="37"/>
+        <location filename="../src/settingswindow.ui" line="51"/>
         <source>Main</source>
         <translation>Général</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="49"/>
+        <location filename="../src/settingswindow.ui" line="63"/>
         <source>Protocol</source>
         <translation>Protocole</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="55"/>
+        <location filename="../src/settingswindow.ui" line="69"/>
         <source>UDP
 The fastest protocol available.</source>
         <translation>UDP
 Le plus rapide.</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="63"/>
+        <location filename="../src/settingswindow.ui" line="77"/>
         <source>UDP (lower MTU)
 Slightly slower than the first one, but required by a few ISPs.</source>
         <translation>UDP (MTU plus petit)
 Légèrement moins rapide, mais requis par certains FAI.</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="71"/>
+        <location filename="../src/settingswindow.ui" line="85"/>
         <source>TCP
 The slowest, but less likely to be filtered. Please try UDP first.</source>
         <translation>TCP
 Plus lent, mais plus rarement bloqué. Essayez l&apos;UDP d&apos;abord.</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="85"/>
+        <location filename="../src/settingswindow.ui" line="99"/>
         <source>Enable IPv6 tunnel</source>
         <translation>Activer le tunnel IPv6</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="96"/>
+        <location filename="../src/settingswindow.ui" line="109"/>
+        <source>Start on boot</source>
+        <translation>Lancer au démarrage du système</translation>
+    </message>
+    <message>
+        <location filename="../src/settingswindow.ui" line="123"/>
+        <source>Connect on start:</source>
+        <translation>Connecter au lancement:</translation>
+    </message>
+    <message>
+        <location filename="../src/settingswindow.ui" line="131"/>
         <source>Advanced</source>
         <translation>Avancé</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="102"/>
+        <location filename="../src/settingswindow.ui" line="137"/>
         <source>HTTP Proxy to use:</source>
         <translation>Proxy HTTP à utiliser:</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="109"/>
+        <location filename="../src/settingswindow.ui" line="144"/>
         <source>Leave empty to use none</source>
         <translation>Laisser vide pour ne pas en utiliser</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="116"/>
+        <location filename="../src/settingswindow.ui" line="151"/>
         <source>DNS server for API queries: (without VPN)</source>
         <translation>Serveur DNS pour contacter l&apos;API: (sans VPN)</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="123"/>
+        <location filename="../src/settingswindow.ui" line="158"/>
         <source>Leave empty to use the default server</source>
         <translation>Laisser vide pour utiliser le serveur par défaut</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="130"/>
+        <location filename="../src/settingswindow.ui" line="165"/>
         <source>Override DNS server: (while connected to the VPN)</source>
         <translation>Serveur DNS: (avec le VPN)</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="137"/>
+        <location filename="../src/settingswindow.ui" line="172"/>
         <source>Leave empty to use the VPN&apos;s internal server</source>
         <translation>Laisser vide pour utiliser le serveur interne du VPN</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="145"/>
+        <location filename="../src/settingswindow.ui" line="210"/>
+        <source>Uninstall</source>
+        <translation>Désinstaller</translation>
+    </message>
+    <message>
+        <location filename="../src/settingswindow.ui" line="217"/>
+        <source>Forget saved password</source>
+        <translation>Oublier les mots de passe</translation>
+    </message>
+    <message>
+        <location filename="../src/settingswindow.ui" line="240"/>
+        <source>Additional OpenVPN configuration:</source>
+        <translation>Configuration additionnelle d&apos;OpenVPN:</translation>
+    </message>
+    <message>
+        <location filename="../src/settingswindow.ui" line="257"/>
         <source>About</source>
         <translation>À propos</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="156"/>
+        <location filename="../src/settingswindow.ui" line="268"/>
         <source>LVPN GUI</source>
-        <translation type="unfinished"></translation>
+        <translation>LVPN GUI</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="174"/>
+        <location filename="../src/settingswindow.ui" line="286"/>
         <source>Version 1.2.3-4</source>
-        <translation type="unfinished"></translation>
+        <translation>Version 1.2.3-4</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="189"/>
+        <location filename="../src/settingswindow.ui" line="301"/>
         <source>LVPNGUI by PacketImpact</source>
-        <translation type="unfinished"></translation>
+        <translation>LVPNGUI par PacketImpact</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="216"/>
+        <location filename="../src/settingswindow.ui" line="40"/>
         <source>Cancel</source>
         <translation>Annuler</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="223"/>
+        <location filename="../src/settingswindow.ui" line="33"/>
         <source>Ok</source>
         <translation>Ok</translation>
     </message>
     <message>
-        <location filename="../settingswindow.ui" line="14"/>
-        <location filename="../settingswindow.cpp" line="16"/>
+        <location filename="../src/settingswindow.ui" line="14"/>
+        <location filename="../src/settingswindow.cpp" line="39"/>
         <source>Settings</source>
         <translation>Paramètres</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="19"/>
+        <location filename="../src/settingswindow.cpp" line="42"/>
         <source>Version %1</source>
         <translation>Version %1</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="21"/>
+        <location filename="../src/settingswindow.cpp" line="44"/>
         <source>%1 by %2</source>
         <translation>%1 par %2</translation>
+    </message>
+    <message>
+        <location filename="../src/settingswindow.cpp" line="96"/>
+        <source>Disabled</source>
+        <translation>Désactivé</translation>
     </message>
 </context>
 <context>
     <name>VPNGUI</name>
     <message>
-        <location filename="../vpngui.cpp" line="103"/>
+        <location filename="../src/vpngui.cpp" line="110"/>
+        <source>%1 is already running.</source>
+        <translation>%1 est déjà en cours de fonctionnement.</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="113"/>
         <source>Connect</source>
         <translation>Connecter</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="104"/>
+        <location filename="../src/vpngui.cpp" line="114"/>
         <source>Disconnect</source>
         <translation>Déconnecter</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="105"/>
+        <location filename="../src/vpngui.cpp" line="115"/>
         <source>View Log</source>
         <translation>Voir les logs</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="106"/>
+        <location filename="../src/vpngui.cpp" line="116"/>
         <source>Settings</source>
         <translation>Paramètres</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="107"/>
+        <location filename="../src/vpngui.cpp" line="117"/>
         <source>Quit</source>
         <translation>Quitter</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="124"/>
+        <location filename="../src/vpngui.cpp" line="133"/>
         <source>Installed</source>
         <translation>Installé</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="125"/>
+        <location filename="../src/vpngui.cpp" line="134"/>
         <source>%1 is now installed! (version %2)</source>
         <translation>%1 a bien été installé ! (version %2)</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="312"/>
+        <location filename="../src/vpngui.cpp" line="335"/>
         <source>Connected</source>
         <translation>Connecté</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="312"/>
+        <location filename="../src/vpngui.cpp" line="335"/>
         <source>VPN successfully connected.</source>
         <translation>Connexion au VPN réussie.</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="318"/>
+        <location filename="../src/vpngui.cpp" line="337"/>
+        <source>Connecting</source>
+        <translation>Connexion</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="337"/>
+        <source>VPN connecting...</source>
+        <translation>VPN en cours de connexion...</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="339"/>
+        <source>Disconnecting</source>
+        <translation>Déconnexion</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="339"/>
+        <source>VPN disconnecting...</source>
+        <translation>VPN en cours de déconnexion...</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="341"/>
         <source>Disconnected</source>
         <translation>Déconnecté</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="318"/>
+        <location filename="../src/vpngui.cpp" line="341"/>
         <source>VPN disconnected.</source>
         <translation>VPN déconnecté.</translation>
     </message>
     <message>
-        <location filename="../vpngui.cpp" line="400"/>
+        <location filename="../src/vpngui.cpp" line="420"/>
+        <source>Are you sure you want to uninstall %1 and delete the configuration?</source>
+        <translation>Êtes-vous sûr de voulour désintaller %1 et supprimer la configuration ?</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="422"/>
+        <location filename="../src/vpngui.cpp" line="435"/>
+        <source>Uninstall</source>
+        <translation>Désinstaller</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="436"/>
+        <source>%1 has been uninstalled.</source>
+        <translation>%1 a bien été désinstallé.</translation>
+    </message>
+    <message>
+        <location filename="../src/vpngui.cpp" line="450"/>
         <source>Gateways update error</source>
         <translation>Erreur lors de la mise à jour de la liste des serveurs</translation>
     </message>
